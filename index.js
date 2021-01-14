4e86c75232f9670a2af07ecd2aa45297d1511ef9
Jakob Persons <jakobep24@gmail.com>
2021-01-14 09:05:56 -0500

    redid the lab to brush up

--- index.js
+++ index.js
@@ -21,3 +21,35 @@ function destructivelyDeleteFromDriverByKey(driver, key) {
    delete driver[key];
    return driver;
 }
+
+
+// REDO
+
+
+// function updateDriverWithKeyAndValue(obj, key, value) {
+//    return Object.assign({}, obj,{[key]: value})
+// }
+
+// function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+//    obj[key] = value;
+//    return obj;
+// }
+
+// function deleteFromDriverByKey(obj, key) {
+//    const newObj = Object.assign({}, obj);
+//    delete newObj[key];
+//    return newObj;
+// }
+
+// function destructivelyDeleteFromDriverByKey(obj, key) {
+//    delete obj[key];
+//    return obj;
+// }
+
+// console.log(updateDriverWithKeyAndValue(driver, 'jake', '2012 subaru legacy'))
+// console.log(driver)
+// console.log(destructivelyUpdateDriverWithKeyAndValue(driver, 'brynn', '2021 jeep cherokee'))
+// console.log(destructivelyUpdateDriverWithKeyAndValue(driver, 'jake', '2012 subaru legacy'))
+// console.log(deleteFromDriverByKey(driver, 'brynn'))
+// console.log(destructivelyDeleteFromDriverByKey(driver, 'jake'))
+// console.log(driver)
